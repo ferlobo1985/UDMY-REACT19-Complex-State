8ba95923b96e0251af04eb20a8aa817a38ed9f4b
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-13 15:24:05 -0300

    06-Extra reducers part 1
    06-Extra reducers part 1

--- src/store/users.js
+++ src/store/users.js
@@ -1,4 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+
+export const fetchUsers = createAsyncThunk(
+    'users/fetchUsers',
+    async(thunkAPI)=>{
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users').then(response => response.data);
+        return res;
+    }
+)
+
 
 export const usersSlice =  createSlice({
     name:'users',
@@ -10,6 +20,19 @@ export const usersSlice =  createSlice({
         setType:(state,action)=>{
             state.type = action.payload || 'Guest'
         }
+    },
+    extraReducers:(builder)=> {
+        builder
+        .addCase(fetchUsers.pending,(state)=>{
+            console.log('Pending')
+        })
+        .addCase(fetchUsers.fulfilled,(state,action)=>{
+            console.log('fulfilled');
+            state.users = action.payload
+        })
+        .addCase(fetchUsers.rejected,(state)=>{
+            console.log('Rejected')
+        })
     }
 });
 
